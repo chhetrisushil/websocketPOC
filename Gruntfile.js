676775d9166785ac93a06a9965eb3cacaa4167a4
roguechhetri <sushil@zlemma.com>
2014-11-05 23:51:12 +0530

    adding grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+/*
+ * Gruntfile.js
+ * Copyright (C) 2014 sushil <sushil@zlemma.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+module.exports = function (grunt) {
+  grunt.initConfig({});
+};
