df1bf1cc84ee48a6275e367c4663308469bc1fdc
Nikhil Kulkarni <nikhil@zlemma.com>
2014-11-14 21:51:35 +0530

    websocket experimentaion files

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 var application_root = __dirname,
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    errorHandler = require('express-error-handler'),
-    ejs = require('ejs'),
-    path = require('path'),
-    router = require('./router.js'),
-    server = express();
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('express-error-handler'),
+  ejs = require('ejs'),
+  path = require('path'),
+  router = require('./router.js'),
+  server = express();
 
 server.use(bodyParser());
 server.use(methodOverride());
-server.use(errorHandler({ dumpExceptions: true, showStack: true }));
+server.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
 server.use(express.static('client/static/'));
 server.set('views', path.join(application_root, '..', '/client'));
 server.engine('html', ejs.renderFile);
